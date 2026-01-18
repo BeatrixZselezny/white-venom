2f40a76b4b4a9fbd1435e2995fec5bcc63235db7
Beatrix Zselezny <cattybaby75@gmail.com>
2026-01-18 20:13:04 +0100

    We created: SafeExecutor, SECURITY_BOOTSTRAP_NOTES.md

---
+++ backup/SafeExecutor.hpp
@@ -0,0 +1,22 @@
+// © 2026 Beatrix Zselezny. All rights reserved.
+// White-Venom Security Framework
+
+#ifndef SAFE_EXECUTOR_HPP
+#define SAFE_EXECUTOR_HPP
+
+#include <string>
+#include <vector>
+#include "utils/ExecPolicyRegistry.hpp"
+
+namespace Venom::Core {
+    class SafeExecutor {
+    public:
+        /**
+         * @brief A "Prepared Statement" logika: bináris és argumentum vektor szétválasztva.
+         * Megfelel a projekt biztonsági előírásainak (fork/execv).
+         */
+        static bool execute(const std::string& binary, const std::vector<std::string>& args);
+    };
+}
+
+#endif
